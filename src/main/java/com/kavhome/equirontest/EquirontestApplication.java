cb93924d61728297d69ea5f44a70cda395b0a5aa
Kolyuchkin Aleksandr <koljuchkin.aleksandr@alphaopen.com>
2021-04-06 22:22:17 +0300

    Init project

---
+++ src/main/java/com/kavhome/equirontest/EquirontestApplication.java
@@ -0,0 +1,13 @@
+package com.kavhome.equirontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquirontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquirontestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kavhome/equirontest/EquirontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavhome.equirontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquirontestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
