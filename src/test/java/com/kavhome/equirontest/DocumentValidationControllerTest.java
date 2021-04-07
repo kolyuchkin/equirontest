80544c2cde3fe78ff465b6164268e00360a20c44
Kolyuchkin Aleksandr <koljuchkin.aleksandr@alphaopen.com>
2021-04-07 14:04:45 +0300

    First Part with Test

---
+++ src/test/java/com/kavhome/equirontest/DocumentValidationControllerTest.java
@@ -0,0 +1,55 @@
+package com.kavhome.equirontest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kavhome.equirontest.controller.DocumentValidationController;
+import com.kavhome.equirontest.pojo.DocPojo;
+import com.kavhome.equirontest.pojo.ProductPojo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author <a href="mailto:koljuchkin.aleksandr@alphaopen.com" >Aleksandr Kolyuchkin</a>
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = DocumentValidationController.class)
+public class DocumentValidationControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void whenInputIsInvalid_thenReturnsStatus400() throws Exception {
+        DocPojo input = new DocPojo();
+        input.setSeller("111111111");
+        input.setCustomer("222222222");
+        final ProductPojo pojo1 = new ProductPojo();
+        pojo1.setName("sdfsdf");
+        pojo1.setCode("3333333333333");
+        input.setProducts(List.of(pojo1));
+        String body = objectMapper.writeValueAsString(input);
+
+        mvc.perform(post("/validate")
+                .contentType("application/json")
+                .content(body))
+                .andExpect(status().isOk());
+
+        input.setProducts(List.of());
+        body = objectMapper.writeValueAsString(input);
+
+        mvc.perform(post("/validate")
+                .contentType("application/json")
+                .content(body))
+                .andExpect(status().isBadRequest());
+    }
+}
